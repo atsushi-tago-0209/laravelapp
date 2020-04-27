9808e6f97c2b9fff0fc63413b5c6e9ec1466d35b
atsushi-tago-0209 <0209at@gmail.com>
2020-04-27 15:29:38 +0900

    session

---
+++ resources/views/rest/create.blade.php
@@ -0,0 +1,18 @@
+    <form action="/rest" method="post">
+      <table>
+        @csrf
+        <tr>
+          <th>message: </th>
+          <td><input type="text" name="message" value="{{old('message')}}"></td>
+        </tr>
+        <tr>
+          <th>url: </th>
+          <td><input type="text" name="url" value="{{old('url')}}"></td>
+        </tr>
+
+        <tr>
+          <th></th>
+        </tr>
+        <td><input type="submit" value="send"></td>
+      </table>
+    </form>
